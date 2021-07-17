11ed7b31ed7bddc7c3918f4cc9458d60ba897292
Business Goose <44676012+business-goose@users.noreply.github.com>
2021-07-18 00:10:55 +0100

    Update (#1)
    * Stop evaluating on leave and add /eval stop and /eval execute
    
    * Stop evaluating on leave and add /eval stop and /eval execute
    
    * Stop evaluating on leave and add /eval stop and /eval execute
    
    * Stop evaluating on leave and add /eval stop and /eval execute
    
    * Stop evaluating on leave and add /eval stop and /eval execute
    
    * Stop evaluating on leave and add /eval stop and /eval execute and patch empty messages
    
    * Reformat code

--- src/main/java/me/allink/fabriceval/commands/EvalCommand.java
+++ src/main/java/me/allink/fabriceval/commands/EvalCommand.java
@@ -3,38 +3,44 @@ package me.allink.fabriceval.commands;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import me.allink.fabriceval.FabricEval;
 import me.allink.fabriceval.threads.EvalThread;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.argument.MessageArgumentType;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
 
 public class EvalCommand implements Command<FabricClientCommandSource> {
     public static MinecraftClient client = MinecraftClient.getInstance();
     public static List<String> lines = new ArrayList<>();
-    static public Timer timer = new Timer();
+    public static List<EvalThread> threads = new ArrayList<>();
 
     public static int eval(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
         String shellCommand = context.getArgument("command", MessageArgumentType.MessageFormat.class).getContents();
         EvalThread evalThread = new EvalThread(shellCommand);
+        threads.add(evalThread);
         evalThread.start();
         return 1;
     }
 
     @Override
     public int run(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
-        Text text = Text.of("You must give a command to execute!");
-        Style style = text.getStyle();
-        style = style.withExclusiveFormatting(Formatting.RED);
-        text = text.getWithStyle(style).get(0);
+        Text text = Text.of("/eval execute <command>\n/eval stop");
         context.getSource().sendFeedback(text);
-
         return 0;
+    }
+
+    public static int stop(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
+        EvalCommand.lines = new ArrayList<>();
+        for(EvalThread thread: threads) {
+            thread.interrupt();
+        }
+        context.getSource().sendFeedback(Text.of("Stopped evaluation."));
+
+        FabricEval.killMessageTimerTask();
+        return 1;
     }
 }
